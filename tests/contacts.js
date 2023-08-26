20a9554a165c0c9c761dbe8549f8f01720aa6cc7
Stéphane Klein <contact@stephane-klein.info>
2023-08-26 18:28:55 +0200

    Add first sql unittest

--- tests/contacts.js
+++ tests/contacts.js
@@ -1,6 +1,22 @@
 import postgres from 'postgres';
 
-console.log(postgres);
-test('Insert in contacts', () => {
-    expect(2 + 2).toBe(4);
+let sql;
+
+
+beforeAll(() => {
+    sql = postgres(
+        'postgres://postgrestest:passwordtest@localhost:5433/myapp'
+    );
+});
+afterAll(() => {
+    sql.end()
+});
+
+test('Insert in contacts', async () => {
+    await sql`DELETE FROM main.contacts`;
+    await sql`INSERT INTO main.contacts (firstname, lastname) VALUES('John', 'Doe')`;
+
+    expect(
+        (await sql`SELECT COUNT(*)::INTEGER FROM main.contacts WHERE firstname = 'John'`)[0].count
+    ).toBe(1);
 });
